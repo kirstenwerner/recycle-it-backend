086abb30018416ea14c2c916e2bd6d72c2c21557
kirstenwerner <kirstenmwerner@gmail.com>
2019-07-19 14:26:38 -0700

    initial commit with new api

--- app/controllers/recycle_center_controller.rb
+++ app/controllers/recycle_center_controller.rb
@@ -2,9 +2,17 @@ class RecycleCenterController < ApplicationController
 
 
   def index
-    conn = Faraday::Connection.new 'https://data.wprdc.org/dataset/10dd50cf-bf29-4268-83e2-debcacea7885/resource/cdb6c800-3213-4190-8d39-495e36300263/download/wasterecoveryimg.geojson'
-    @recycle_center = conn.get ''
-    render json: JSON.parse(@recycle_center.body)
+    conn = Faraday.new(
+      url: "http://api.earth911.com"
+    )
+
+    resp = conn.get("/earth911.searchLocations") do |req|
+      req.params["api_key"] = "bce31bab12e1c92c"
+      req.params["latitude"] = 47.60
+      req.params["longitude"] = -122.33
+    end
+
+    render json: JSON.parse(resp.body)
   end
 
 end
