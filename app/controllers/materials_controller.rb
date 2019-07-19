b91c5851b74c2ac92b821d6316c565f8e9c1c3ef
kirstenwerner <kirstenmwerner@gmail.com>
2019-07-19 14:31:33 -0700

    materials get request and JSON route

---
+++ app/controllers/materials_controller.rb
@@ -0,0 +1,15 @@
+class Materials < ApplicationController
+
+  def index
+    conn = Faraday.new(
+      url: "http://api.earth911.com"
+    )
+
+    resp = conn.get("/earth911.getMaterials") do |req|
+      req.params["api_key"] = "bce31bab12e1c92c"
+    end
+
+    render json: JSON.parse(resp.body)
+  end
+
+end
